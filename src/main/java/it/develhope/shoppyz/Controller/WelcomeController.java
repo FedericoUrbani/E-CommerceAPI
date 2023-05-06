f14da4b059c318fbe18ecb5c8ef1f3f77c3ae10a
federico urbani <urbanifederico5@gmail.com>
2023-05-07 01:52:55 +0200

    add dependecy for thymeleaf and tested landingpage controller

--- src/main/java/it/develhope/shoppyz/Controller/WelcomeController.java
+++ src/main/java/it/develhope/shoppyz/Controller/WelcomeController.java
@@ -1,20 +1,19 @@
 package it.develhope.shoppyz.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/Shoppyz")
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+
+@Controller
 public class WelcomeController {
 
-    @GetMapping
-    public String welcomePage(){
-        return "Welcome in shoppyz!\n" +
-                "\n" +
-                "Use the following Api Endpoints\n" +
-                "\n" +
-                "http://localhost:8080/Shoppyz/account";
+    /* Mapping for welcome page */
+    @GetMapping("/Shoppyz")
+    public String sayHello (Model model){
+        model.addAttribute("theDate", new java.util.Date());
+        return "welcomePage";
     }
 }
