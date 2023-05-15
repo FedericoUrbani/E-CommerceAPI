35e98f9a17dcb0382a15c9f7d2f62d3571581ad8
federico urbani <urbanifederico5@gmail.com>
2023-05-15 15:20:30 +0200

    order status implementation

---
+++ src/main/java/it/develhope/shoppyz/order/OrderStatus.java
@@ -0,0 +1,14 @@
+package it.develhope.shoppyz.order;
+
+public enum OrderStatus {
+
+    OrderCancelled,
+    OrderDelivered,
+    OrderInTransit,
+    OrderPaymentDue,
+    OrderPickupAvailable,
+    OrderProblem,
+    OrderProcessing,
+    OrderReturned
+
+}
